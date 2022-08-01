4858cf9816addf164a8218e0016eb0e7f0ba09e9
efraga <efragafloresd@gmail.com>
2022-08-01 08:44:16 -0500

    [INIT]: Create repository

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Extra Fields",
+    'summary': """
+        Add new extra fields product to extend attributes
+    """,
+    'author': "efraga",
+    'website': "https://www.leyfra.com",
+    'category': 'Uncategorized',
+    'version': '14.0.0.0.1',
+    'depends': [
+        'base',
+        'product'
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/product_form.xml',
+    ],
+}
